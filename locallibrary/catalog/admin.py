8dbfdad9dfd3f5fe86eaf075936e51e8278e8d20
tbaslios <baslioty@dukes.jmu.edu>
2025-02-16 10:07:10 -0500

    part 5 complete

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,17 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+
 from .models import Author, Genre, Book, BookInstance, Language
 
-#admin.site.register(Book)
-#admin.site.register(Author)
+# admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
 admin.site.register(Genre)
-#admin.site.register(BookInstance)
+
+# next line because I completed the challenge in the tutorial
 admin.site.register(Language)
 
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+# Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
 
 # Register the admin class with the associated model
@@ -19,19 +28,20 @@ admin.site.register(Author, AuthorAdmin)
 
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
+    extra = 0
 
+# Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
-
+    list_display = ("title", "author", "display_genre")
     inlines = [BooksInstanceInline]
-
 
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_filter = ('status', 'due_back')
+    list_display = ("book", "status", "due_back", "imprint", "id")
+    list_filter = ("status", "due_back")
 
     fieldsets = (
         (None, {
@@ -41,4 +51,3 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('status', 'due_back')
         }),
     )
-
