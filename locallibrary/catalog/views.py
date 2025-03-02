8ac9237565be471d16427918966fef17ec0e9c7f
tbaslios <baslioty@dukes.jmu.edu>
2025-03-02 13:31:39 -0500

    late fix on views.py

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,23 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """View function for home page of site."""
-
-    # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
 
-    # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
-    # The 'all()' is implied by default.
-    def index(request):
-        num_authors = Author.objects.count()  # The 'all()' is implied by default.
+    num_authors = Author.objects.count() 
 
-    # Number of visits to this view, as counted in the session variable.
     num_visits = request.session.get('num_visits', 0)
     num_visits += 1
     request.session['num_visits'] = num_visits
@@ -29,17 +22,13 @@ def index(request):
         'num_authors': num_authors,
         'num_visits': num_visits,
     }
-
-    # Render the HTML template index.html with the data in the context variable.
     return render(request, 'index.html', context=context)
-
-
-from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 10
+    paginate_by = 10  
 
 class BookDetailView(generic.DetailView):
     model = Book
+
 
